2f1309cefcf1f750c30fa5cfd7bd25c0620b9205
Alex Taylor <alexmctaylorpants@gmail.com>
2016-06-16 22:43:31 -0700

    Make commentCreeper an object for more flexibility

--- comment-creeper/creeper.js
+++ comment-creeper/creeper.js
@@ -1,50 +1,57 @@
-const escapeHtml = function(string) {
-  if (typeof string !== 'string') {
-    console.warn(string)
-    throw `string expected, got ${typeof string} instead`
-  }
-  // thanks - http://bit.ly/1YsJt6b
-  const chars = {
-     '&': '&amp;',
-      '<': '&lt;',
-      '>': '&gt;',
-      '"': '&quot;',
-      "'": '&#039;'
-  }
-  return string.replace(/[&<>"']/g, function(m) { return chars[m]; });
+const commentsCreeper = {
+  escapeHtml: function(string) {
+    if (typeof string !== 'string') {
+      console.warn(string)
+      throw `string expected, got ${typeof string} instead`
+    }
+    // thanks - http://bit.ly/1YsJt6b
+    const chars = {
+       '&': '&amp;',
+        '<': '&lt;',
+        '>': '&gt;',
+        '"': '&quot;',
+        "'": '&#039;'
+    }
+    return string.replace(/[&<>"']/g, function(m) { return chars[m]; });
+  },
+
+
+  commentNode: function(content) {
+    return `<div class="commentspy-commentNode">${this.escapeHtml(content)}</div>`
+  },
+
+  comments: function() {
+    return $(document).find('*').
+    map(function() {
+      return $(this).contents().
+        filter(function() {
+          return this.nodeType === 8
+        }).
+        map(function() {
+          return this.nodeValue
+        }).
+        toArray()
+    }).
+    toArray()
+  }(),
+
+  documentComments: function() {
+    return [...document.childNodes].
+    filter(function(node) {
+      return node.nodeType === 8
+    }).
+    map(function(node) {
+      return node.nodeValue
+    })
+  }(),
+
+  buildComments: function() {
+    const nodes = [...this.comments, ...this.documentComments]
+    nodes.map(function(comment) {
+      $('body').prepend(this.commentNode(comment))
+    }.bind(this))
+  },
 }
 
+commentsCreeper.buildComments()
 
-const commentNode = function(content) {
-  return `<div class="commentspy-commentNode">${escapeHtml(content)}</div>`
-}
-
-
-const buildComments = function(nodes) {
-  nodes.map(function(comment) {
-    $('body').prepend(commentNode(comment))
-  })
-}
-
-let $comments = $(document).find('*').
-  map(function() {
-    return $(this).contents().
-      filter(function() {
-        return this.nodeType === 8
-      }).
-      map(function() {
-        return this.nodeValue
-      }).
-      toArray()
-  }).
-  toArray()
-
-let $documentComments = [...document.childNodes].
-  filter(function(node) {
-    return node.nodeType === 8
-  }).
-  map(function(node) {
-    return node.nodeValue
-  })
-
-buildComments([...$comments, ...$documentComments])
