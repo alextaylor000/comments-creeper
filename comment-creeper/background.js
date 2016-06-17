67f1f0183e1ef4ee540d04c1a2803df11daf8e25
Alex Taylor <alexmctaylorpants@gmail.com>
2016-06-16 22:43:41 -0700

    Improvements to browser action

--- comment-creeper/background.js
+++ comment-creeper/background.js
@@ -1,11 +1,15 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
-  chrome.tabs.executeScript({
-    file: 'vendor/jquery-3.0.0.min.js'
-  })
   chrome.tabs.insertCSS({
     file: 'creeper.css'
   });
   chrome.tabs.executeScript({
-    file: 'creeper.js'
+    file: 'vendor/jquery-3.0.0.min.js'
+  })
+  chrome.tabs.executeScript({
+    file: 'creeper.js',
+    runAt: 'document_idle'
   });
+
+  // we don't want it to run twice and create duplicate comments
+  chrome.browserAction.disable(tab.id)
 });
