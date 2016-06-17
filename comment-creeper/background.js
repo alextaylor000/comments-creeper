1a363de86855a0cd7cca9cee969d33145daa5ee2
Alex Taylor <alexmctaylorpants@gmail.com>
2016-06-16 21:34:07 -0700

    Add styles

--- comment-creeper/background.js
+++ comment-creeper/background.js
@@ -2,6 +2,9 @@ chrome.browserAction.onClicked.addListener(function(tab) {
   chrome.tabs.executeScript({
     file: 'vendor/jquery-3.0.0.min.js'
   })
+  chrome.tabs.insertCSS({
+    file: 'creeper.css'
+  });
   chrome.tabs.executeScript({
     file: 'creeper.js'
   });
